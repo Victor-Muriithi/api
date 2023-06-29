d9dc61331274f4b764a4f7eb2672afa01e9d1ad8
Victor-Muriithi <vmuriithi10@gmail.com>
2023-06-29 12:04:22 +0300

    fourth commit

--- index.js
+++ index.js
@@ -10,40 +10,40 @@ app.get("/", function (req, res) {
     res.send('Hello World now!')
   })
 
-app.post('/ussd', (req, res) => {
-    // Read the variables sent via POST from our API
-    const {
-        sessionId,
-        serviceCode,
-        phoneNumber,
-        text,
-    } = req.body;
+// app.post('/ussd', (req, res) => {
+//     // Read the variables sent via POST from our API
+//     const {
+//         sessionId,
+//         serviceCode,
+//         phoneNumber,
+//         text,
+//     } = req.body;
 
-    let response = '';
+//     let response = '';
 
-    if (text == '') {
-        // This is the first request. Note how we start the response with CON
-        response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
-    } else if ( text == '1') {
-        // Business logic for first level response
-        response = `CON Choose account information you want to view
-        1. Account number`;
-    } else if ( text == '2') {
-        // Business logic for first level response
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if ( text == '1*1') {
-        // This is a second level response where the user selected 1 in the first instance
-        const accountNumber = 'ACC100101';
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your account number is ${accountNumber}`;
-    }
+//     if (text == '') {
+//         // This is the first request. Note how we start the response with CON
+//         response = `CON What would you like to check
+//         1. My account
+//         2. My phone number`;
+//     } else if ( text == '1') {
+//         // Business logic for first level response
+//         response = `CON Choose account information you want to view
+//         1. Account number`;
+//     } else if ( text == '2') {
+//         // Business logic for first level response
+//         // This is a terminal request. Note how we start the response with END
+//         response = `END Your phone number is ${phoneNumber}`;
+//     } else if ( text == '1*1') {
+//         // This is a second level response where the user selected 1 in the first instance
+//         const accountNumber = 'ACC100101';
+//         // This is a terminal request. Note how we start the response with END
+//         response = `END Your account number is ${accountNumber}`;
+//     }
 
-    // Send the response back to the API
-    res.set('Content-Type: text/plain');
-    res.send(response);
-});
+//     // Send the response back to the API
+//     res.set('Content-Type: text/plain');
+//     res.send(response);
+// });
 
-app.listen(5000 || process.env.PORT)
+// app.listen(5000 || process.env.PORT)
