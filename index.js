6b277d0f68f86b97d2b52e373c542fe4c84d1194
Victor-Muriithi <vmuriithi10@gmail.com>
2023-06-29 11:36:08 +0300

    test 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 console.log(`here ${process.env.PORT}`)
-app.get("/test", function (req, res) {
+app.get("/", function (req, res) {
     res.send('Hello World now!')
   })
 
